0b09bdd92ebc43c0338e715200032683a17dcba2
KarryOn1 <zeeshanagondal@gmail.com>
2024-04-01 12:06:36 -0400

    Added updateAccBalance function,  and addCredit to make it easier to update values correspondingly. Modified Credits.js to account for the implementations. Added gitignore to ignore node modules

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -4,57 +4,56 @@ src/components/Credits.js
 The Credits component contains information for Credits page view.
 Note: You need to work on this file for the Assignment.
 ==================================================*/
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import AccountBalance from "./AccountBalance";
 
-class Credits extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      accountBalance: this.props.accountBalance,
-      creditList: this.props.creditList || [], // Initialize with an empty array if creditList is undefined
+const Credits = (props) => {
+  const { credits, balance, updateAccBalance } = props;
+
+  // Create the list of Credit items
+  let newBalance = 0;
+
+  const roundToTwo = (value) => {
+    return Math.round(value * 100) / 100;
+  };
+
+  // Create the list of Credit items
+  const addCredit = (event) => {
+    event.preventDefault();
+    newBalance = roundToTwo(
+      balance + parseFloat(event.target.amount.value)
+    );
+
+    const newCredit = {
+      id: credits.length + 1,
+      description: event.target.description.value,
+      amount: parseFloat(event.target.amount.value).toFixed(2),
+      date: new Date().toISOString(),
     };
-  }
-  
 
-  creditView = () => {
-    const listofCredits = this.state.creditList.map((credit, index) => (
-      <li style={{ listStylePosition: 'inside' }} key={index}>
-        Description: {credit.description} || Amount: ${credit.amount} || Date:{' '}
+    updateAccBalance(newBalance);
+    props.addCredit(newCredit);
+    event.target.reset();
+  };
+
+  let creditsView = () => {
+    const listofCredits = credits.map((credit, index) => (
+      <li style={{ listStylePosition: "inside" }} key={index}>
+        Description: {credit.description} || Amount: ${credit.amount} || Date:{" "}
         {credit.date.slice(0, 10)}
       </li>
     ));
     return <ul>{listofCredits}</ul>;
   };
 
-  // add credit function
-  addCredit = (event) => {
-    event.preventDefault();
-    const newCredit = {
-      description: event.target.description.value,
-      amount: event.target.amount.value,
-      date: new Date().toISOString(),
-    };
-    const updatedCredits = [...this.state.creditList, newCredit];
-    const newBalance =
-      parseFloat(this.state.accountBalance) + parseFloat(newCredit.amount);
-    this.setState({
-      accountBalance: newBalance.toFixed(2),
-      creditList: updatedCredits,
-    });
-    event.target.reset(); // Reset the form fields after submission
-  };
+  return (
+    <div>
+      <h1>Credits</h1>
+      {creditsView()}
+      <br />
 
-  render() {
-    return (
-      <div>
-        <h1>Credits</h1>
-        <br />
-        <div>Credits List: {this.creditView()}</div>
-        <div>Balance: ${this.state.accountBalance}</div>
-        <br />
-        <form onSubmit={this.addCredit}>
-          <label>
+      <form onSubmit={addCredit}>
+      <label>
             Description: <input type="text" name="description" required />
           </label>
           <br />
@@ -62,15 +61,16 @@ class Credits extends Component {
             Amount: <input type="number" step="0.01" name="amount" required />
           </label>
           <br />
-          <button type="submit">Add Credit</button>
-        </form>
-        <br />
-        <Link to="/debits">Debits</Link>
-        <br />
-        <Link to="/">Return to Home</Link>
-      </div>
-    );
-  }
-}
+        <button type="submit">Add Credit</button>
+      </form>
+      <br />
+      <AccountBalance accountBalance={balance} />
+      <br />
+      <Link to="/debits">View Debits</Link>
+      <br />
+      <Link to="/">Return to Home</Link>
+    </div>
+  );
+};
 
 export default Credits;
